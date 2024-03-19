b53c3e4296ccb2d968f9d8452cd7f9589e92645f
Towsif Hossain <towsifhossain5120@gmail.com>
2024-03-19 15:39:22 +0600

    update and delete works

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,4 +20,39 @@ class PostController extends Controller
         Post::create($incomingFields);
         return redirect('/');
     }
+
+    public function showEditScreen(Post $post) {
+        if (auth()->id() !== $post['user_id']) {
+            return redirect('/');
+        }
+        return view('edit-post', ['post' => $post]);
+    }
+
+    public function updatePost(Post $post, Request $request) {
+        if (auth()->id() !== $post['user_id']) {
+            return redirect('/');
+        }
+
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+
+        return redirect('/');
+
+    }
+
+    public function deletePost(Post $post) {
+        if (auth()->id() !== $post['user_id']) {
+            return redirect('/');
+        }
+
+        $post->delete();
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
 
 Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'updatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
